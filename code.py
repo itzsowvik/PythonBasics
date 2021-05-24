a2d124abfc1390e8af97650749385655d8cb14db
Sowvik Sarker <ssowvik@gmail.com>
2021-05-24 22:33:44 +0600

    looping

--- code.py
+++ code.py
@@ -1,8 +1,22 @@
-name = input("Enter your name: ")
+name_list = []
+for i in range (5):
+    val = input("Enter Name: ")
+    name_list.append(val)
 
-age =  input("Enter your Age: ")
+print(name_list)
 
-if(age.isdigit()):
-    age = int(age)
-else:
-    print("Age must be number!!")
+choice = input("Do you Want to remove someone from list?: ")
+
+if(choice == "Yes"):
+    to_remove = input("Enter your choice: ")
+    for i in range (len(name_list)-1):
+        if(name_list[i] == to_remove):
+            name_list.remove(name_list[i])
+            print("Name Removed")
+            break
+        else:
+            print("Name Not Found on the list")
+
+
+print(name_list)
+"""print(type(name_list))"""
