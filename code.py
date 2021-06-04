9a89a763bc6a6741e9ea21ccaa5959d0b27fe1d5
Sowvik Sarker <ssowvik@gmail.com>
2021-06-04 22:13:08 +0600

    Function

--- code.py
+++ code.py
@@ -1,22 +1,7 @@
-name_list = []
-for i in range (5):
-    val = input("Enter Name: ")
-    name_list.append(val)
-
-print(name_list)
-
-choice = input("Do you Want to remove someone from list?: ")
-
-if(choice == "Yes"):
-    to_remove = input("Enter your choice: ")
-    for i in range (len(name_list)-1):
-        if(name_list[i] == to_remove):
-            name_list.remove(name_list[i])
-            print("Name Removed")
-            break
-        else:
-            print("Name Not Found on the list")
+def name_input():
+    data = input("What's Your Name?: ")
+    return data;
 
 
-print(name_list)
-"""print(type(name_list))"""
+res = name_input();
+print(res)
